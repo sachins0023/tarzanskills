b6b523c1fb845ded391347874a25a81eada260c9
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-05 13:39:14 +0530

    Print statements with comments in day-2 evaluated

--- day-2.py
+++ day-2.py
@@ -1,14 +1,14 @@
-print("I will now count my chickens: ")
-print("Hens", 25+30/6)
-print("Roosters",100-25*3%4)
-print("Now I will count the eggs: ")
-print(3+2+1-5+4%2-1/4+6)
-print("Is it true that 3+2<5-7?")
-print(3+2<5-7)
-print("What is 3+2?",3+2)
-print("What is 5-7?",5-7)
-print("Oh, that's why it's False.")
-print("How about some more.")
-print("Is it greater?",5>-2)
-print("Is it greater or equal",5>=-2)
-print("Is it less or equal?",5<=-2)
+print("I will now count my chickens: ") #I will now count my chickens:
+print("Hens", 25+30/6)                  #Hens 30.0
+print("Roosters",100-25*3%4)            #Roosters 97
+print("Now I will count the eggs: ")    #Now I will count the eggs:
+print(3+2+1-5+4%2-1/4+6)                #6.75
+print("Is it true that 3+2<5-7?")       #Is it true that 3+2<5-7?
+print(3+2<5-7)                          #False
+print("What is 3+2?",3+2)               #What is 3+2? 5
+print("What is 5-7?",5-7)               #What is 5-7? -2
+print("Oh, that's why it's False.")     #Oh,that's why it's False
+print("How about some more.")           #How about some more.
+print("Is it greater?",5>-2)            #Is it greater? True
+print("Is it greater or equal?",5>=-2)  #Is it greater or equal? True
+print("Is it less or equal?",5<=-2)     #Is it less or equal? False
