85495512e5e30bfd15c650f04ecd81999b96a063
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-05 13:56:12 +0530

    Print statements with more arithmetic problems

--- day-2.py
+++ day-2.py
@@ -12,3 +12,8 @@ print("How about some more.")           #How about some more.
 print("Is it greater?",5>-2)            #Is it greater? True
 print("Is it greater or equal?",5>=-2)  #Is it greater or equal? True
 print("Is it less or equal?",5<=-2)     #Is it less or equal? False
+print((25+3)*75.3)                      #2108.4
+print((35/3)*(25+4.9))                  #348.833333333333333
+from decimal import Decimal
+print((Decimal('42.2')+Decimal('37.403'))*(Decimal('7')%Decimal('4')+Decimal('3')-Decimal('2')*Decimal('7')))       #-636.824
+print(Decimal('-636.8240000000001')+Decimal('636.824'))
