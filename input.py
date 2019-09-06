a54c0048f3096fe8c8809ebc156488384b74eea2
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-06 12:10:23 +0530

    Program to input my details

---
+++ input.py
@@ -0,0 +1,9 @@
+#Prints the name, salary and age inputted
+
+first_name=input("Enter First name: ")
+last_name=input("Enter Last name: ")
+age=input("Enter your age: ")
+salary=input("enter your salary: ")
+print("Your name is", first_name, last_name)
+print("Your age is", age)
+print("Your salary is", salary)
