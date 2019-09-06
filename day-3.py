1f742652dace0993b5525faa7eae0306d92d16b2
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-06 10:56:18 +0530

    string operations with repeating,joining and including parts of string

---
+++ day-3.py
@@ -0,0 +1,14 @@
+string = 'abcac'
+length = 12
+string_length= len(string)
+times= length//string_length
+remaining= length%string_length
+print(string*times+string[0:remaining])
+print(string*times+string[0:4])
+print(string*times+string[0:8])
+print(string*times+string[0:-1])
+print(string*times+string[-1:0])
+print(string*times+string[-1:4])
+print(string*times+string[-3:0])
+print(string[-5:-1])
+print(string[::-1])
