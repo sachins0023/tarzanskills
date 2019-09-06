f78463e291bdf24012083ed8739789c7fdbfcdae
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-06 13:39:42 +0530

    Program to input my details with particular data type

--- input.py
+++ input.py
@@ -4,6 +4,21 @@ first_name=input("Enter First name: ")
 last_name=input("Enter Last name: ")
 age=input("Enter your age: ")
 salary=input("enter your salary: ")
-print("Your name is", first_name, last_name)
-print("Your age is", age)
-print("Your salary is", salary)
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(salary))
+
+if type(first_name) == str and type(last_name) == str:
+    print("Your name is", first_name, last_name)
+else:
+    print("Entered value not a name")
+if type(age) == int:
+    print("Your age is", age)
+else:
+    print("Entered value not age")
+if type(salary) == float or type(salary) == int:
+    print("Your salary is", salary)
+else:
+    print("Entered value is not salary")
