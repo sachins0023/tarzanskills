a231063cdd9c3562bbe6749f54f6302a32df1489
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-07 13:08:19 +0530

    All day 2 drills are completed

--- day2_exercise.py
+++ day2_exercise.py
@@ -1,3 +1,33 @@
+print("Hello, World!")
+print("Hello Again!")
+print("I like typing like this.")
+print("This is fun.")
+print('Yay! Printing')
+print("I'd much rather you 'not'")
+print('I "said" do not touch this')
+
+print("I will now count my chickens: ") #I will now count my chickens:
+print("Hens", 25+30/6)                  #Hens 30.0
+print("Roosters",100-25*3%4)            #Roosters 97
+print("Now I will count the eggs: ")    #Now I will count the eggs:
+print(3+2+1-5+4%2-1/4+6)                #6.75
+print("Is it true that 3+2<5-7?")       #Is it true that 3+2<5-7?
+print(3+2<5-7)                          #False
+print("What is 3+2?",3+2)               #What is 3+2? 5
+print("What is 5-7?",5-7)               #What is 5-7? -2
+print("Oh, that's why it's False.")     #Oh,that's why it's False
+print("How about some more.")           #How about some more.
+print("Is it greater?",5>-2)            #Is it greater? True
+print("Is it greater or equal?",5>=-2)  #Is it greater or equal? True
+print("Is it less or equal?",5<=-2)     #Is it less or equal? False
+
+print(8/2.0)
+print(8.0/2)
+print(9/4)
+print(9//4)
+print(2++2)
+
+
 name = 'Zed A. Shaw'
 age = 35 # not a lie
 height = 74 # inches
